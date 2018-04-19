eb802c2c0931c9f2d9c230049bdde11dbfaa563e
DAUDIN <DAUDIN@172.18.196.159>
2018-04-19 16:31:24 +0200

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,20 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			VBox root;
+			FXMLLoader rootLoader = new FXMLLoader(getClass().getResource("view/Editeur - Accueil.fxml"));
+			root=rootLoader.load();
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
