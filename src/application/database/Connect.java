88ad04976b4daa1b2d905c7707a702f42bcba7f1
Baptiste <Baptiste@172.18.208.115>
2018-05-15 11:57:39 +0200

    classe connect bdd editeur

--- src/application/database/Connect.java
+++ src/application/database/Connect.java
@@ -3,22 +3,32 @@ package application.database;
 import java.sql.*;
 
 public class Connect {
-	
+
 	Statement stt;
 	PreparedStatement pst;
 	Connection con;
-	
+
 	public Connect(String chemin, String nom, String mdp) throws SQLException {
-		con = DriverManager.getConnection("jdbc:h2:"+chemin,nom,mdp);
+		con = DriverManager.getConnection("jdbc:h2:" + chemin, nom, mdp);
 		stt = con.createStatement();
 	}
-	
+
 	public ResultSet executeCmd(String requete) throws SQLException {
 		pst = con.prepareStatement(requete);
-        ResultSet res = pst.executeQuery();
-        System.out.println(res);
-        System.out.println(res);
-        return res;
+		ResultSet res = pst.executeQuery();
+		return res;
+	}
+
+	public void Affichage(ResultSet res, String nom) throws SQLException {
+		while (res.next()) {
+			System.out.println(res.getString(nom));
+		}
+
+	}
+
+	public void Disconnect() throws SQLException {
+		con.close();
+
 	}
 
 }
